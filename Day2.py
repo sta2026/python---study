1ccf0798ebbe7ba1b3450cfad9a98e74bbe21d69
sta2026 <sta202622@gmail.com>
2026-01-28 23:23:45 +0900

    Day1, Day2 파이썬 기초 학습

---
+++ Day2.py
@@ -0,0 +1,48 @@
+age = 20
+if age >=20:
+    print("성인입니다")
+else:
+    print("성인이 아닙니다")
+score = 85
+if score >= 90:
+    grade = 'A'
+elif score >= 80:
+    grade = 'B'
+elif score >= 70:
+    grade = 'C'
+elif score >= 60:
+    grade = 'D'
+else:
+    grade = 'F'
+print("학점은", grade, "입니다")
+number = 7
+if number > 0:
+    a = "양수"
+elif number < 0:
+    a = "음수"
+else: 
+    a = "0"
+print(a)
+age = 11
+if age> 20:
+    b = "성인"
+else:
+    b = "미성년자"
+score = 75
+if score >=90:
+    grade ="A"
+elif score >=80:
+    grade = "B"
+elif score >=70:
+    grade = "c"
+elif score >=60:
+    grade = "D"
+else:
+    grade = "F"
+print("학점은", grade, "입니다")
+
+num = 7
+if num % 2 ==0:
+    print("짝수입니다")
+else:
+    print("홀수입니다")
